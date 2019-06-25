7b97ec89172197d43d08f5147ef7ade313ab35f3
Baron Xiao <bx334@nyu.edu>
2019-06-24 23:34:48 -0400

    Add cors config

--- HistoricalMysteryAPI/ServicesExtensions.cs
+++ HistoricalMysteryAPI/ServicesExtensions.cs
@@ -1,7 +1,16 @@
-﻿namespace HistoricalMysteryAPI
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HistoricalMysteryAPI
 {
     public static class ServicesExtensions
     {
-
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+            });
+        }
     }
 }
