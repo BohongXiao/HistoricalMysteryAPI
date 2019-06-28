01de4e4250a4000b068bc0d8153f653e172d4915
Baron Xiao <bx334@nyu.edu>
2019-06-27 20:56:00 -0400

    Create DI module for each library, make DI decoupled

--- HistoricalMysteryAPI/ServiceExtensions.cs
+++ HistoricalMysteryAPI/ServiceExtensions.cs
@@ -1,8 +1,5 @@
-﻿using HM.Common.Utils;
+﻿using HM.Common;
 using HM.DataAccess.DB;
-using HM.Interfaces.IRepositories;
-using HM.Interfaces.IServices;
-using HM.Interfaces.IUtils;
 using HM.LogicContainers;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -24,19 +21,9 @@ namespace HistoricalMysteryAPI
 
         public static void ConfigureDependencyInjections(this IServiceCollection services)
         {
-            services.AddSingleton<ILoggingService, LoggingService>();
+            services.ConfigureCommonDi();
             services.ConfigureRepositoryDi();
             services.ConfigureServicesDi();
-        }
-
-        public static void ConfigureRepositoryDi(this IServiceCollection services)
-        {
-            services.AddScoped<ITestRepo, TestRepo>();
-        }
-
-        public static void ConfigureServicesDi(this IServiceCollection services)
-        {
-            services.AddScoped<ITestService, TestService>();
         }
     }
 }
